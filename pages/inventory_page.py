e777cb2bd3749e84cef4e40a39fc82a03e8c9398
Alexandra-Balabolkina <alexandrabalabolkina@gmail.com>
2024-09-12 18:08:02 +0200

    Added all files from the pages folder

---
+++ pages/inventory_page.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+
+class InventoryPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.add_to_cart_button = (By.ID, 'add-to-cart-sauce-labs-onesie')
+        self.cart_icon = (By.CLASS_NAME, 'shopping_cart_link')
+
+    def add_to_cart(self):
+        # Добавление товара в корзину
+        self.driver.find_element(*self.add_to_cart_button).click()
+
+    def go_to_cart(self):
+        # Переход в корзину
+        self.driver.find_element(*self.cart_icon).click()
